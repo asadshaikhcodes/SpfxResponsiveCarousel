698e1e79dbd5e2bb27fe9d1d41fd26a71ac5bc1e
Asad Shaikh <ashaikh@aqltech.com>
2021-02-15 23:32:05 +0530

    created project files

---
+++ src/webparts/spfxResponsiveCarousel/components/ISpfxResponsiveCarouselProps.ts
@@ -0,0 +1,3 @@
+export interface ISpfxResponsiveCarouselProps {
+  description: string;
+}
---
+++ src/webparts/spfxResponsiveCarousel/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxResponsiveCarouselWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxResponsiveCarouselWebPartStrings' {
+  const strings: ISpfxResponsiveCarouselWebPartStrings;
+  export = strings;
+}
